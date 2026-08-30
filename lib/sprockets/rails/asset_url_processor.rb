29739afc59fe4ac4b53b39d63e8233fc4fd9c11a
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2026-08-30 08:25:16 +0200

    Resolve quoted CSS asset URLs safely

--- lib/sprockets/rails/asset_url_processor.rb
+++ lib/sprockets/rails/asset_url_processor.rb
@@ -2,11 +2,16 @@ module Sprockets
   module Rails
     # Resolve assets referenced in CSS `url()` calls and replace them with the digested paths
     class AssetUrlProcessor
-      REGEX = /url\(\s*["']?(?!(?:\#|data:|http[s]?:))(?<relativeToCurrentDir>\.\/)?(?<path>[^"'\s)]+)\s*["']?\)/
+      REGEX = /url\(\s*(?:"(?<double_quoted>[^"]*)"|'(?<single_quoted>[^']*)'|(?<unquoted>[^"'\s)]+))\s*\)/
+      URI_REFERENCE = /\A(?:\#|\/\/|[a-z][a-z0-9+.-]*:)/i
+
       def self.call(input)
         context = input[:environment].context_class.new(input)
-        data    = input[:data].gsub(REGEX) do |_match|
-          path = Regexp.last_match[:path]
+        data    = input[:data].gsub(REGEX) do |match|
+          path = Regexp.last_match.values_at(:double_quoted, :single_quoted, :unquoted).compact.first
+          path = path.delete_prefix('./')
+          next match if path.empty? || path.match?(URI_REFERENCE)
+
           "url(#{context.asset_path(path)})"
         end
 
